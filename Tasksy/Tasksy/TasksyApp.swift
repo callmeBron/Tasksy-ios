3d04cdd0d1280d38a9bc097b102ae7721583f378
Bronwyn dos Santos <development.bronwyn@gmail.com>
2025-02-01 08:05:57 +0200

    Commit #1: Initial Project Commit. Adding the project to the repository.

---
+++ Tasksy/Tasksy/TasksyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TasksyApp.swift
+//  Tasksy
+//
+//  Created by Bronwyn dos Santos on 2025/02/01.
+//
+
+import SwiftUI
+
+@main
+struct TasksyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
