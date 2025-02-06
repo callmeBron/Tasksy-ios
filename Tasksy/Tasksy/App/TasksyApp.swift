e01817db644e8e064b934d7a1c5a16342a82dd8e
Bronwyn dos Santos <development.bronwyn@gmail.com>
2025-02-06 20:07:56 +0200

    Updated the project

--- Tasksy/Tasksy/App/TasksyApp.swift
+++ Tasksy/Tasksy/App/TasksyApp.swift
@@ -3,8 +3,10 @@ import Swinject
 
 @main
 struct TasksyApp: App {
-    var container = TaskContainer()
-    let taskView = TaskContainer.shared.injectObject(AnyView.self, ObjectNames.TaskObjects.taskView)
+    var container = DependencyContainer()
+    let locationManager = LocationManager()
+    
+    let taskView = DependencyContainer.shared.injectObject(AnyView.self, ObjectNames.TaskObjects.taskView)
     var body: some Scene {
         WindowGroup {
             taskView
