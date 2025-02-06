ab97fcf9d29abc7c857e5140b20deb2c4c3844cd
Bronwyn dos Santos <development.bronwyn@gmail.com>
2025-02-06 20:03:59 +0200

    Added the weather Repository and Service layer and subsequent models

--- Tasksy/Tasksy/DataBase/WeatherDatabase.swift
+++ Tasksy/Tasksy/DataBase/WeatherDatabase.swift
@@ -1,2 +1,43 @@
 import Foundation
-/// responsible for storing the response back from the service will communicate with the repository
+import RealmSwift
+
+protocol WeatherDatabase {
+    func persistResponse(response: WeatherResponseModel)
+    func fetch() -> WeatherDataModel?
+}
+
+class RealmWeatherDatabase: WeatherDatabase {
+    func persistResponse(response: WeatherResponseModel){
+        do {
+            let realm = try Realm()
+            try realm.write {
+                let weatherObject = WeatherPersistedModel(responseModel: response)
+                realm.add(weatherObject)
+            }
+        } catch {
+            print("Failed to persist object to realm")
+        }
+    }
+    
+    func fetch() -> WeatherDataModel? {
+        do {
+            let realm = try Realm()
+            return realm.objects(WeatherPersistedModel.self)
+                .compactMap { self.setupWeatherModel(with: $0) }
+                .first
+        } catch {
+            print("Failed to fetch objects from realm")
+            return nil
+        }
+    }
+    
+    private func setupWeatherModel(with weather: WeatherPersistedModel) -> WeatherDataModel? {
+        return WeatherDataModel(location: "\(String(describing: weather.locationInformation?.area ?? "")), \(String(describing: weather.locationInformation?.country ?? ""))",
+                                sunriseTime: weather.forecastInformation?.forecastDay[0].sunriseTime ?? "",
+                                sunsetTime:  weather.forecastInformation?.forecastDay[0].sunsetTime ?? "",
+                                weatherCondition: WeatherCondition(rawValue: weather.currentWeather?.condition ?? "") ?? .sunny,
+                                tempC: String(weather.currentWeather?.tempC ?? 0.0),
+                                feelslikeC: String(weather.currentWeather?.feelslikeC ?? 0.0),
+                                humidity: String(weather.currentWeather?.humidity ?? 0))
+    }
+}
--- Tasksy/Tasksy/Repository/WeatherRepository.swift
+++ Tasksy/Tasksy/Repository/WeatherRepository.swift
@@ -1,35 +1,47 @@
-//import Foundation
-///// responsible to fetch from either the data base(realm) or make the service call again, will communicate with the viewModel
-//
-//class WeatherRepository {
-////    private let dataBase: WeatherDataBase
-//    private let webService = WeatherServiceAPI() // inject dependency
-//    
-//    init() {
-//        
-//    }
-//    
-//    func fetchWeatherData() -> WeatherResponseModel? {
-//        guard let storedDate = UserDefaults.standard.object(forKey: "lastUpdateTime") as? Date else {
-//            return fetchWeatherServiceData()
-//        }
-//        
-//        let calendar = Calendar.current
-//        
-//        if let thirtyMinutesAgo = calendar.date(byAdding: .minute, value: -30, to: Date()),
-//           storedDate >= thirtyMinutesAgo {
-//            return fetchWeatherFromDatabase()
-//        } else {
-//            return fetchWeatherServiceData()
-//        }
-//    }
-//    
-//   private func fetchWeatherServiceData() -> WeatherResponseModel? {
-//       UserDefaults.standard.set(Date(), forKey: "lastUpdateTime")
-//       return nil
-//    }
-//    
-//    private func fetchWeatherFromDatabase() -> WeatherResponseModel? {
-//        return nil
-//    }
-//}
+import Foundation
+/// responsible to fetch from either the data base(realm) or make the service call again, will communicate with the viewModel
+protocol WeatherRepository {
+    func fetchWeatherData() async -> (weatherData: WeatherDataModel?,error: ErrorResponse?)
+}
+
+class ConcreteWeatherRepository: WeatherRepository {
+    private let dataBase: WeatherDatabase
+    private let webService: WeatherServiceAPI
+    
+    init(dataBase: WeatherDatabase, webService: WeatherServiceAPI) {
+        self.dataBase = dataBase
+        self.webService = webService
+    }
+    
+    func fetchWeatherData() async -> (weatherData: WeatherDataModel?, error: ErrorResponse?) {
+        guard let storedDate = UserDefaults.standard.object(forKey: "lastUpdateTime") as? Date else {
+            return await fetchWeatherServiceData()
+        }
+        
+        let calendar = Calendar.current
+        
+        if let thirtyMinutesAgo = calendar.date(byAdding: .minute, value: -30, to: Date()),
+           storedDate >= thirtyMinutesAgo {
+            return (fetchWeatherFromDatabase(), nil)
+        } else {
+            return await fetchWeatherServiceData()
+        }
+    }
+    
+    private func fetchWeatherServiceData() async -> (WeatherDataModel?, ErrorResponse?) {
+        UserDefaults.standard.set(Date(), forKey: "lastUpdateTime")
+        let response = await webService.fetchWeatherInformation()
+        switch response {
+        case .success(let weatherData):
+            dataBase.persistResponse(response: weatherData)
+            let weatherModel = dataBase.fetch()
+            return (weatherModel, nil)
+        case .failure(let error):
+            return (nil, error)
+        }
+    }
+    
+    private func fetchWeatherFromDatabase() -> WeatherDataModel? {
+        return dataBase.fetch()
+    }
+}
